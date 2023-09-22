17b6f21ef67a55a3b46b7bc420feb2d62890473b
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-09-22 18:23:14 +0300

    Database created

--- Shopping/src/main/java/com/example/shopping/model/entity/OrderEntity.java
+++ Shopping/src/main/java/com/example/shopping/model/entity/OrderEntity.java
@@ -1,7 +1,9 @@
 package com.example.shopping.model.entity;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,12 +19,50 @@ public class OrderEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@ManyToOne
 	private UserEntity customer;
-	
-	@OneToMany
-	private List<OrderItemEntity> items;
-	
 
+	@OneToMany(mappedBy = "shoppingOrder")
+	private List<OrderItemEntity> items;
+
+	@Column
+	private LocalDate date;
+
+	public OrderEntity() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public UserEntity getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(UserEntity customer) {
+		this.customer = customer;
+	}
+
+	public List<OrderItemEntity> getItems() {
+		return items;
+	}
+
+	public void setItems(List<OrderItemEntity> items) {
+		this.items = items;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public OrderEntity setDate(LocalDate date) {
+		this.date = date;
+		return this;
+	}
 }
--- Shopping/src/main/java/com/example/shopping/model/entity/OrderItemEntity.java
+++ Shopping/src/main/java/com/example/shopping/model/entity/OrderItemEntity.java
@@ -4,7 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -13,7 +14,43 @@ public class OrderItemEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	@OneToOne
+
+	@ManyToOne
 	private ProductEntity product;
+
+	@JoinColumn(name = "shopping_order")
+	@ManyToOne
+	private OrderEntity shoppingOrder;
+
+	public OrderItemEntity() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public OrderItemEntity setId(Long id) {
+		this.id = id;
+		return this;
+	}
+
+	public ProductEntity getProduct() {
+		return product;
+	}
+
+	public OrderItemEntity setProduct(ProductEntity product) {
+		this.product = product;
+		return this;
+	}
+
+	public OrderEntity gethoppingOrder() {
+		return shoppingOrder;
+	}
+
+	public OrderItemEntity setShoppingOrder(OrderEntity shoppingOrder) {
+		this.shoppingOrder = shoppingOrder;
+		return this;
+	}
+
 }
