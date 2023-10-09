b821f7da61c1fd2b2b2f6e74758fef9877c35cf4
Strahil Hristov <strahil1998@abv.bg>
2023-10-09 20:20:07 +0300

    product with json 1

---
+++ Shopping/src/main/resources/static/js/processing.js
@@ -0,0 +1,15 @@
+fetch("http://localhost:8080/api/products")
+    .then(response => response.json())
+    .then(function (processing) {
+        let placeholder = document.querySelector('#info');
+        let out = "";
+
+        for (const product of processing) {
+            out += `<tr>
+<td><img src="../static/${product.image}"></td>
+<td>${product.productName}</td>
+<td>${product.price}</td>
+</tr>`;
+        }
+        placeholder.innerHTML = out;
+    })
