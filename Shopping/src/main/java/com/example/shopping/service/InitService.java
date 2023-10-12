683f3e95591c8ec428fc0db09bf7c88e9e69f3dd
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-10-12 14:11:57 +0300

    fix

--- Shopping/src/main/java/com/example/shopping/service/InitService.java
+++ Shopping/src/main/java/com/example/shopping/service/InitService.java
@@ -43,8 +43,7 @@ public class InitService {
 				this.shoppingItemRepository.deleteAll();
 				this.specificationsRepositor.deleteAll();
 				this.productRepository.deleteAll();
-				
-				
+
 				products = this.productRepository.saveAll(products);
 
 				for (ProductEntity product : products) {
@@ -57,23 +56,5 @@ public class InitService {
 		} catch (IOException e) {
 			System.out.println("File does't exist!");
 		}
-//		CategoryEntity cat = categoryRepository.findById(5l).get();
-//
-//		ProductEntity prod = new ProductEntity();
-//		prod.setPrice(BigDecimal.valueOf(1234)).setProductName("ASUS").setQuantity(15).setDescription("Good Laptop")
-//				.setCategory(cat);
-//
-//		prod = this.productRepository.save(prod);
-//
-//		SpecificationsEntity spec1 = new SpecificationsEntity("Ram", "8GB", prod);
-//		SpecificationsEntity spec2 = new SpecificationsEntity("CPU", "INTEL CORE I7", prod);
-//		SpecificationsEntity spec3 = new SpecificationsEntity("GPU", "NVIDIA RTX 4060", prod);
-//
-//		this.specificationsRepositor.saveAll(List.of(spec1, spec2, spec3));
-//
-//		this.specificationsRepositor.findAllByProductId(prod.getId())
-//				.forEach(p -> System.out.println(p.getName() + " -> " + p.getValue()));
-
 	}
-
 }
