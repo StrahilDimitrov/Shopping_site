7bcb9eba50dcb6371d8f5b792821601d6d45ced5
Strahil Hristov <strahil1998@abv.bg>
2023-10-13 12:15:53 +0300

    processing

--- Shopping/src/main/resources/static/js/prodInfo.js
+++ Shopping/src/main/resources/static/js/prodInfo.js
@@ -1,5 +1,6 @@
 const backendLocation = 'http://localhost:8080/'
 let productId = document.getElementById("productId").getAttribute("value");
+let counter = 0;
 
 let placeholder = document.getElementById('information');
 
@@ -9,16 +10,29 @@ fetch(backendLocation + "api/product/info/" + productId)
 
 		let out = "";
 
-		out += `<table>
-			<tr>
+		out += `<tr>
 				<th id = "pictures"><img src="${productInfo.image}" /></th>
 				<th id = "productName"><stong>${productInfo.productName}</strong>
 					</br></br>
 					<span id = "productPrice">${productInfo.price} лв.</span>
 				</th>
-			</tr>
-		</table>`
+			</tr>`
 
-		console.log(productInfo);
+		for (const element of productInfo.specs) {
+			if(counter % 2 === 0){
+				out += `<tr id = "color">
+					<th>${element.name}</th>
+					<th>${element.value}</th>
+				</tr>
+			</table>`
+			}
+			else{
+				out += `<tr>
+					<th>${element.name}</th>
+					<th>${element.value}</th>
+				</tr>`
+			}
+			counter++;
+		}
 		placeholder.innerHTML = out;
 	})
