d26166f0dd157eb7f46728c3a571e0801b36d482
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-10-17 23:30:26 +0300

    Test for user service

--- Shopping/src/main/java/com/example/shopping/model/dto/RegisterFormDto.java
+++ Shopping/src/main/java/com/example/shopping/model/dto/RegisterFormDto.java
@@ -1,68 +1,71 @@
 package com.example.shopping.model.dto;
 
 public class RegisterFormDto {
-    private String firstName;
+	private String firstName;
 
-    private String lastName;
+	private String lastName;
 
-    private String email;
+	private String email;
 
-    private String password;
+	private String password;
 
-    private String phoneNumber;
+	private String phoneNumber;
 
-    public RegisterFormDto() {
+	public RegisterFormDto() {
 
-    }
+	}
 
-    public RegisterFormDto(String firstName, String lastName, String email, String password, String phoneNumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-        this.phoneNumber = phoneNumber;
-    }
+	public RegisterFormDto(String firstName, String lastName, String email, String password, String phoneNumber) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.phoneNumber = phoneNumber;
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public RegisterFormDto setFirstName(String firstName) {
-        this.firstName = firstName;
-        return this;
-    }
+	public RegisterFormDto setFirstName(String firstName) {
+		this.firstName = firstName;
+		return this;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public RegisterFormDto setLastName(String lastName) {
-        this.lastName = lastName;
-        return this;
-    }
+	public RegisterFormDto setLastName(String lastName) {
+		this.lastName = lastName;
+		return this;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public RegisterFormDto setEmail(String email) {
+		this.email = email;
+		return this;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public RegisterFormDto setPassword(String password) {
+		this.password = password;
+		return this;
+	}
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+	public RegisterFormDto setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+		return this;
+	}
 
 }
--- Shopping/src/test/java/com/example/shopping/web/rest/ProductsRestControllerTest.java
+++ Shopping/src/test/java/com/example/shopping/web/rest/ProductsRestControllerTest.java
@@ -38,7 +38,9 @@ public class ProductsRestControllerTest {
 			.andExpect(jsonPath("$.[0].productName", is("ACER")))
 			.andExpect(jsonPath("$.[1].productName", is("ASUS")))
 			.andExpect(jsonPath("$.[0].id", is(1)))
-			.andExpect(jsonPath("$.[1].id", is(1)));
+			.andExpect(jsonPath("$.[1].id", is(1)))
+			.andExpect(jsonPath("$.[0].price", is(1500)))
+			.andExpect(jsonPath("$.[1].price", is(2000)));
 	}
 
 	private ProductViewDto createProduct(String name, int price) {
