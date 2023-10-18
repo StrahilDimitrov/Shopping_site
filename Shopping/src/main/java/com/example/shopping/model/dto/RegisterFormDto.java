88457e972d465a3a217e0f06fd392da7987f59b1
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-10-19 01:47:59 +0300

    clearing unused imports

--- Shopping/src/main/java/com/example/shopping/model/dto/RegisterFormDto.java
+++ Shopping/src/main/java/com/example/shopping/model/dto/RegisterFormDto.java
@@ -1,9 +1,6 @@
 package com.example.shopping.model.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public class RegisterFormDto {
