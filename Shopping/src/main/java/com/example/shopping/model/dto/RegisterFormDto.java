c40b9020f19b3238fca867b1a78152ebe1b6cfbe
Strahil Hristov <strahil1998@abv.bg>
2023-10-18 16:09:16 +0300

    redact

--- Shopping/src/main/java/com/example/shopping/model/dto/RegisterFormDto.java
+++ Shopping/src/main/java/com/example/shopping/model/dto/RegisterFormDto.java
@@ -1,14 +1,20 @@
 package com.example.shopping.model.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public class RegisterFormDto {
 	@NotBlank
+	@Min(3)
+	@Max(10)
 	private String firstName;
 
 	@NotBlank
+	@Min(3)
+	@Max(10)
 	private String lastName;
 
 	@Email
