f262bd3ffdb3c725b419c0a2a2d8e460a40afe84
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-10-18 11:53:49 +0300

    Added validations

--- Shopping/src/main/java/com/example/shopping/web/AuthController.java
+++ Shopping/src/main/java/com/example/shopping/web/AuthController.java
@@ -1,9 +1,13 @@
 package com.example.shopping.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.shopping.model.dto.RegisterFormDto;
 import com.example.shopping.service.UserService;
@@ -23,9 +27,14 @@ public class AuthController {
 	}
 
 	@PostMapping("/register")
-	public String register(RegisterFormDto registerForm) {
+	public String register(@Validated RegisterFormDto registerForm, BindingResult bindingResult,
+			RedirectAttributes redirectAttributes, ModelAndView modelAndView) {
+
+		if (bindingResult.hasErrors()) {
+
+		}
 		this.userService.registerUser(registerForm);
-		
+
 		return "Shopping";
 	}
 
