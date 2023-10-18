b92eecfb2ca216bcc424782249b057f1aafa6d61
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-10-18 11:50:10 +0300

    Added validations

--- Shopping/src/main/java/com/example/shopping/model/dto/RegisterFormDto.java
+++ Shopping/src/main/java/com/example/shopping/model/dto/RegisterFormDto.java
@@ -1,14 +1,22 @@
 package com.example.shopping.model.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class RegisterFormDto {
+	@NotBlank
 	private String firstName;
 
+	@NotBlank
 	private String lastName;
 
+	@Email
 	private String email;
 
 	private String password;
 
+	@Size(min = 10)
 	private String phoneNumber;
 
 	public RegisterFormDto() {
--- Shopping/src/main/java/com/example/shopping/model/entity/UserEntity.java
+++ Shopping/src/main/java/com/example/shopping/model/entity/UserEntity.java
@@ -14,110 +14,113 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "users")
 public class UserEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(name = "first_name")
-    private String firstName;
-    @Column(name = "last_name")
-    private String lastName;
-    @Column
-    private String email;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column
+	private String email;
 
-    @Column
-    private String password;
+	@Column
+	private String password;
 
-    @Column(name = "phone_number")
-    private String phoneNumber;
+	@Column(name = "phone_number")
+	private String phoneNumber;
 
-    @OneToMany(mappedBy = "customer")
-    private List<OrderEntity> orders;
+	@OneToMany(mappedBy = "customer")
+	private List<OrderEntity> orders;
 
-    @OneToMany(mappedBy = "user")
-    private List<AddressEntity> deliveryAddresses;
+	@OneToMany(mappedBy = "user")
+	private List<AddressEntity> deliveryAddresses;
 
-    public UserEntity() {
-        orders = new ArrayList<>();
-        deliveryAddresses = new ArrayList<>();
-    }
+	public UserEntity() {
+		orders = new ArrayList<>();
+		deliveryAddresses = new ArrayList<>();
+	}
 
-    public UserEntity(String firstName, String lastName, String email, String password, String phoneNumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-        this.phoneNumber = phoneNumber;
-    }
+	public UserEntity(String firstName, String lastName, String email, String password, String phoneNumber) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.phoneNumber = phoneNumber;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public UserEntity setId(Long id) {
-        this.id = id;
-        return this;
-    }
+	public UserEntity setId(Long id) {
+		this.id = id;
+		return this;
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public UserEntity setFirstName(String firstName) {
-        this.firstName = firstName;
-        return this;
-    }
+	public UserEntity setFirstName(String firstName) {
+		this.firstName = firstName;
+		return this;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public UserEntity setLastName(String lastName) {
-        this.lastName = lastName;
-        return this;
-    }
+	public UserEntity setLastName(String lastName) {
+		this.lastName = lastName;
+		return this;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public UserEntity setEmail(String email) {
-        this.email = email;
-        return this;
-    }
+	public UserEntity setEmail(String email) {
+		this.email = email;
+		return this;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public UserEntity setPassword(String password) {
-        this.password = password;
-        return this;
-    }
+	public UserEntity setPassword(String password) {
+		this.password = password;
+		return this;
+	}
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
 
-    public UserEntity setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-        return this;
-    }
+	public UserEntity setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+		return this;
+	}
 
-    public List<OrderEntity> getOrders() {
-        return orders;
-    }
+	public List<OrderEntity> getOrders() {
+		return orders;
+	}
 
-    public UserEntity setOrders(List<OrderEntity> orders) {
-        this.orders = orders;
-        return this;
-    }
+	public UserEntity setOrders(List<OrderEntity> orders) {
+		this.orders = orders;
+		return this;
+	}
 
-    public List<AddressEntity> getDeliveryAddresses() {
-        return deliveryAddresses;
-    }
+	public List<AddressEntity> getDeliveryAddresses() {
+		return deliveryAddresses;
+	}
 
-    public void setDeliveryAddresses(List<AddressEntity> deliveryAddresses) {
-        this.deliveryAddresses = deliveryAddresses;
-    }
+	public void setDeliveryAddresses(List<AddressEntity> deliveryAddresses) {
+		this.deliveryAddresses = deliveryAddresses;
+	}
 
 }
