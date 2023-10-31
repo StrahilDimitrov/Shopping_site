3d1330db40492ac5af7b651a6490044e43a78c6d
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-10-31 23:51:23 +0200

    Remove unnecessary exception class

--- Shopping/src/main/java/com/example/shopping/exceptions/AccountIsNotActivatedException.java
+++
@@ -1,9 +0,0 @@
-package com.example.shopping.exceptions;
-
-public class AccountIsNotActivatedException extends RuntimeException{
-	private static final long serialVersionUID = 1L;
-
-	public AccountIsNotActivatedException(String message) {
-        super(message);
-    }
-}
