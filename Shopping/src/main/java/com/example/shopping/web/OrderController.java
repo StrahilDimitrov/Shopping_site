a49f011e23211142b6c5384e6743aaee80988d0c
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-11-14 12:03:02 +0200

    commit

--- Shopping/src/main/java/com/example/shopping/web/OrderController.java
+++ Shopping/src/main/java/com/example/shopping/web/OrderController.java
@@ -2,22 +2,28 @@ package com.example.shopping.web;
 
 import com.example.shopping.model.dto.ApplicationUserDetails;
 import com.example.shopping.service.OrderService;
+import com.example.shopping.service.ShoppingCartService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class OrderController {
     private final OrderService orderService;
+    private final ShoppingCartService shoppingCartService;
 
-    public OrderController(OrderService orderService) {
+    public OrderController(OrderService orderService, ShoppingCartService shoppingCartService) {
         this.orderService = orderService;
+        this.shoppingCartService = shoppingCartService;
     }
 
     @GetMapping("/checkout")
-    public String placeOrder(@AuthenticationPrincipal ApplicationUserDetails user) {
-        //TODO
-        return "cardInformation";
+    public ModelAndView placeOrder(@AuthenticationPrincipal ApplicationUserDetails user, ModelAndView modelAndView) {
+        this.shoppingCartService.loadShoppingCart(modelAndView, user);
+        modelAndView.setViewName("cardInformation");
+
+        return modelAndView;
     }
 
 }
