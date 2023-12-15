e834b1cb7b092be5d0b133af856731e2f6e6cbae
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-12-15 23:22:47 +0200

    fix

--- Shopping/src/main/java/com/example/shopping/web/OrderController.java
+++ Shopping/src/main/java/com/example/shopping/web/OrderController.java
@@ -1,29 +1,27 @@
 package com.example.shopping.web;
 
-import com.example.shopping.model.dto.ApplicationUserDetails;
-import com.example.shopping.service.OrderService;
-import com.example.shopping.service.ShoppingCartService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.shopping.model.dto.ApplicationUserDetails;
+import com.example.shopping.service.ShoppingCartService;
+
 @Controller
 public class OrderController {
-    private final OrderService orderService;
-    private final ShoppingCartService shoppingCartService;
+	private final ShoppingCartService shoppingCartService;
 
-    public OrderController(OrderService orderService, ShoppingCartService shoppingCartService) {
-        this.orderService = orderService;
-        this.shoppingCartService = shoppingCartService;
-    }
+	public OrderController(ShoppingCartService shoppingCartService) {
+		this.shoppingCartService = shoppingCartService;
+	}
 
-    @GetMapping("/checkout")
-    public ModelAndView placeOrder(@AuthenticationPrincipal ApplicationUserDetails user, ModelAndView modelAndView) {
-        this.shoppingCartService.loadShoppingCart(modelAndView, user);
-        modelAndView.setViewName("cardInformation");
+	@GetMapping("/checkout")
+	public ModelAndView placeOrder(@AuthenticationPrincipal ApplicationUserDetails user, ModelAndView modelAndView) {
+		this.shoppingCartService.loadShoppingCart(modelAndView, user);
+		modelAndView.setViewName("cardInformation");
 
-        return modelAndView;
-    }
+		return modelAndView;
+	}
 
 }
